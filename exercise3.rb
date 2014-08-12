3475501fd561de337833bb7bff91278e417af55e
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 08:34:09 -0400

    Completed exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,13 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+
+time_now = Time.now
+current_year = time_now.year
+
+year_of_birth = current_year.to_i - age.to_i
+
+puts "I see, you were born in #{year_of_birth}"
