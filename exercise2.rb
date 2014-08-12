432ae72ae02d7470385d04d7f7cdf4e4d8171b42
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 08:23:15 -0400

    Completed exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#Calculating a tip on a $55 meal
+tip_amount = 55 * 0.2
+puts "Your meal was $55, the suggested tip is $#{tip_amount.to_i}"
+
+#Converting integers to strings
+puts "The highway is " + 5.to_s + "km north"
+
+#String interpolation and calculation
+number_a = 45628
+number_b = 7839
+puts "The output of #{number_a} multiplied by #{number_b} is: #{number_a * number_b}"
+
+#Booleans
+(true && false) || (false && true) || !(false && false)
